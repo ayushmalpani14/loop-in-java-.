2d20c27072d846c687759812a99c56f3a61c2ea0
ayushmalpani14 <ayushmalpani014@gmail.com>
2026-06-25 15:54:37 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,12 @@
+class forloop
+{
+    public static void mian(String arg[])
+  
+    {
+         for(int i=2;i<=6;i++)
+        
+            System.out.println("Hi " + i);
+        
+
+    }
+}
---
+++ loop1.java
@@ -0,0 +1,19 @@
+class loop1 
+{ 
+    public static void main (String arg[])
+
+ {
+     int i = 1 ;
+
+     while(i <= 10)
+     {  System.out.println("hello " + i );
+       
+     int j = 1 ;
+       while (j <= 9) {
+        System.out.println("bye" + j);
+        j++ ;
+       } i++ ;
+     }       System.out.println("ayush" + i);
+    } 
+    
+ }
